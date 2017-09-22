ad75842d70d8a7add6b92ef848a107d9cbf53976
Sara Spink <spinkbot@gmail.com>
2017-09-22 15:05:49 -0700

    doesn't work but is beginning to set up routes

--- app.rb
+++ app.rb
@@ -6,3 +6,21 @@ require("./lib/volunteer")
 require("pg")
 
 DB = PG.connect({:dbname => 'volunteer_tracker_test'})
+
+get('/') do
+  @project_list = Project.all()
+  erb(:index)
+end
+
+post('/') do
+  @project_app = params["title"]
+  new_project = Project.new({:id=> @id_app, :title=> @title_app})
+  new_project.save
+  erb(:index)
+end
+
+get('/project/:id') do
+  @project_list = Project.all()
+  @project = Project.find(params[:id])
+  erb(:project_list)
+end
